2da50606829d37d58e8395dce3647a8f18167d2d
tpaxman <tylerpaxman@gmail.com>
2023-04-22 13:28:47 -0600

    move files around

--- run_abebooks_searches.py
+++ run_abebooks_searches.py
@@ -1,12 +1,13 @@
-import abebooks as abe
+import tools.abebooks as abe
 import pandas as pd
 
-input_filename = 'grandpa_meeks_collection.txt'
+input_filename = 'all_books.txt'
 
 with open(input_filename, encoding='utf-8') as f:
     raw_inputs = f.read()
 
 inputs = [x.strip().split(';') for x in raw_inputs.strip().split('\n')]
+inputs_table = pd.DataFrame(inputs, columns=['author_search', 'title_search', 'binding']).set_index(['author_search', 'title_search']).assign(dummy=1).drop(columns='binding')
 
 outputs_list = [abe.run_abebooks_search(author=author, title=title, binding=binding).assign(author_search=author, title_search=title) for author, title, binding in inputs]
 
@@ -19,8 +20,14 @@ aggregated = (outputs
     .sort_values('price_cad')
     .set_index(['title_search', 'author_search'])
     .join(grouped['title'].count().rename('item_count').to_frame())
-    .reset_index(drop=True)
     [['title', 'author', 'seller', 'binding', 'price_cad', 'total_price_cad', 'item_count']]
+)
+
+aggregated_full = (inputs_table
+    .join(aggregated)
+    .drop(columns='dummy')
+    #.reset_index(level='author_search', drop=True)
+    .sort_values('price_cad')
 )
 
 localprices = (outputs
