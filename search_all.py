3a0d5dc4fa9509ab8037f14c0b5a1ec7c3f2a1e3
tpaxman <tylerpaxman@gmail.com>
2023-04-22 22:56:14 -0600

    add argparse to search_all

--- search_all.py
+++ search_all.py
@@ -1,4 +1,14 @@
 from tools import abebooks, bibliocommons, goodreads, annas_archive
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--author", "-a")
+    parser.add_argument("--title", "-t")
+    parser.add_argument("--keywords", "-k")
+    args = parser.parse_args()
+    lookup_everything(author=args.author, title=args.title, keywords=args.keywords)
+
 
 
 def lookup_everything(author: str=None, title: str=None, keywords: str=None):
@@ -8,7 +18,7 @@ def lookup_everything(author: str=None, title: str=None, keywords: str=None):
     datasets = {
         "abebooks": abebooks.run_abebooks_search(author=author, title=title, keywords=keywords)[['title', 'author', 'binding', 'condition', 'seller', 'price_cad', 'total_price_cad']],
         "epl": bibliocommons.search_epl(author=author, title=title, anywhere=keywords)[['title', 'author', 'format_description', 'availability_status', 'hold_counts']],
-        "cpl": bibliocommons.search_cpl(author=author, title=title, anywhere=keywords, formatcode=['EBOOK'])[['title', 'author', 'format_description', 'availability_status', 'hold_counts']],
+        "cpl": bibliocommons.search_cpl(author=author, title=title, anywhere=keywords, formatcode='EBOOK')[['title', 'author', 'format_description', 'availability_status', 'hold_counts']],
         "goodreads": goodreads.run_goodreads_search(search_string)[['title', 'author', 'avg_rating', 'num_ratings']],
         "annas": annas_archive.run_annas_archive_search(search_string=search_string)[['title', 'author', 'filetype', 'filesize_mb']],
     }
@@ -26,4 +36,5 @@ def lookup_everything(author: str=None, title: str=None, keywords: str=None):
 
 
 
-
+if __name__ == '__main__':
+    main()
