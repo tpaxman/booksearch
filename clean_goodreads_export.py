31336776b6b8574bc7e5a58d091d6e7058b7aeb7
tpaxman <tylerpaxman@gmail.com>
2023-05-20 23:15:56 -0600

    update clean_goodreads script to accept shelf filter

--- clean_goodreads_export.py
+++ clean_goodreads_export.py
@@ -6,12 +6,19 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('input_csv')
     parser.add_argument('output_csv')
+    parser.add_argument('--shelf', '-s', help='shelf to filter to')
     args = parser.parse_args()
     input_csv = args.input_csv
     output_csv = args.output_csv
+    shelf = args.shelf
 
     goodreads_raw = pd.read_csv(input_csv)
     goodreads_clean = goodreads.clean_library_export(goodreads_raw)
+    
+    if shelf:
+        shelf_dummies = goodreads.get_shelf_dummies(goodreads_clean)
+        to_read = shelf_dummies.loc[lambda t: t['to-read'], ['goodreads_id']]
+        goodreads_clean = goodreads_clean.merge(to_read)
 
     goodreads_clean.to_csv(output_csv, index=False)
 
